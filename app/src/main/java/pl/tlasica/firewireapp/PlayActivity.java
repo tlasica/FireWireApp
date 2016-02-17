c8acc0c106c3812d4cb7ab8bc509fcf8a4ec5ec3
Tomasz Łasica <tlasica@life.pl>
2016-02-17 00:04:56 -0500

    Icon. Better FullScreen

--- app/src/main/java/pl/tlasica/firewireapp/PlayActivity.java
+++ app/src/main/java/pl/tlasica/firewireapp/PlayActivity.java
@@ -1,7 +1,11 @@
 package pl.tlasica.firewireapp;
 
+import android.app.ActionBar;
+import android.content.pm.ActivityInfo;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 
@@ -32,20 +36,41 @@ public class PlayActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_play);
-        mContentView = findViewById(R.id.content_view);
-        fullScreenMode(mContentView);
+
+        View decorView = getWindow().getDecorView();
+        fullScreenMode(decorView);
 
         mGameView = findViewById(R.id.game_view);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("", "onResume()");
+
+        View decorView = getWindow().getDecorView();
+        fullScreenMode(decorView);
+
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            Log.d("", "Hiding action bar");
+            actionBar.hide();
+        }
+    }
+
     private void fullScreenMode(View view) {
-        view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        //int currentApiVersion = android.os.Build.VERSION.SDK_INT;
+        int flags = View.SYSTEM_UI_FLAG_LOW_PROFILE;
+        flags |= View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+        flags |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        flags |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+        flags |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        flags |= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+        flags |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        view.setSystemUiVisibility(flags);
     }
 
 }
