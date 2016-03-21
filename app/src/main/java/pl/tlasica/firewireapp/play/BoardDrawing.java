33372e923438066d5d9091b1e15c57cc01b89247
Tomasz Łasica <tlasica@life.pl>
2016-03-20 23:59:47 -0400

    Smart diagonal wires drawing

--- app/src/main/java/pl/tlasica/firewireapp/play/BoardDrawing.java
+++ app/src/main/java/pl/tlasica/firewireapp/play/BoardDrawing.java
@@ -8,14 +8,17 @@ import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.RectF;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import pl.tlasica.firewireapp.model.Board;
 import pl.tlasica.firewireapp.model.IntCoord;
 import pl.tlasica.firewireapp.model.LevelPlay;
 import pl.tlasica.firewireapp.model.PlacedConnector;
 import pl.tlasica.firewireapp.model.Wire;
+import scala.Int;
 
 
 public class BoardDrawing extends CanvasDrawing {
@@ -28,6 +31,9 @@ public class BoardDrawing extends CanvasDrawing {
     private Paint boardPaint = fillPaint(boardColor);
     private Paint connectionPaint;
     private Paint bmpPaint = new Paint();
+
+    private Random random = new Random(System.currentTimeMillis());
+    private Map<Integer, Integer> diagonalWires = new HashMap<>();
 
     private Paint[] wirePaints = {
             strokePaint(10, Color.parseColor("#2A5528")),
@@ -44,7 +50,7 @@ public class BoardDrawing extends CanvasDrawing {
 
 
     private int full_wire_size = 8; // full wire consists of 8 parts
-    private int conn_wire_size = 6; // we do not print last part
+    private int conn_wire_size = 4; // we do not print last part
 
     public void draw(Canvas canvas, LevelPlay play) {
         prepareDrawing(canvas);
@@ -58,12 +64,12 @@ public class BoardDrawing extends CanvasDrawing {
     }
 
     public int nodeNumber(Point mouse, Board board) {
-        for(int n: board.nodes) {
+        for (int n : board.nodes) {
             float x = canvasX(IntCoord.x(n));
             float y = canvasY(IntCoord.y(n));
             float dist = cellSize / 3;
-            boolean xFit = (mouse.x>=x-dist && mouse.x<=x+dist);
-            boolean yFit = (mouse.y>=y-dist && mouse.y<=y+dist);
+            boolean xFit = (mouse.x >= x - dist && mouse.x <= x + dist);
+            boolean yFit = (mouse.y >= y - dist && mouse.y <= y + dist);
             if (xFit && yFit) {
                 return n;
             }
@@ -72,7 +78,7 @@ public class BoardDrawing extends CanvasDrawing {
     }
 
     void drawConnectors(Canvas canvas, Board board, Map<Integer, PlacedConnector> placedConnectors) {
-        for(Map.Entry<Integer, PlacedConnector> item: placedConnectors.entrySet()) {
+        for (Map.Entry<Integer, PlacedConnector> item : placedConnectors.entrySet()) {
             int pos = item.getKey();
             PlacedConnector conn = item.getValue();
             drawConnector(canvas, board, pos, conn);
@@ -80,7 +86,7 @@ public class BoardDrawing extends CanvasDrawing {
     }
 
     void drawNodes(Canvas canvas, Board board) {
-        for(Integer n: board.nodes) {
+        for (Integer n : board.nodes) {
             drawNode(canvas, n, true);
         }
     }
@@ -99,7 +105,7 @@ public class BoardDrawing extends CanvasDrawing {
         wirePaints[0].setStrokeWidth(wireWidth + border);
         wirePaints[1].setStrokeWidth(wireWidth);
         wirePaints[2].setStrokeWidth(wireWidth - border);
-        for(Wire w: board.wires) {
+        for (Wire w : board.wires) {
             drawWire(canvas, w.a, w.b, wirePaints[0], full_wire_size);
             drawWire(canvas, w.a, w.b, wirePaints[1], full_wire_size);
             drawWire(canvas, w.a, w.b, wirePaints[2], full_wire_size);
@@ -113,36 +119,65 @@ public class BoardDrawing extends CanvasDrawing {
         float by = canvasY(IntCoord.y(b));
         float xStep = (bx - ax) / full_wire_size;
         float yStep = (by - ay) / full_wire_size;
-        boolean horizontal = (ax==bx);
-        boolean vertical = (by==ay);
+        boolean horizontal = (ax == bx);
+        boolean vertical = (by == ay);
         if (horizontal || vertical) {
             canvas.drawLine(ax, ay, ax + length * xStep, ay + length * yStep, paint);
-        }
-        else {
-            // draw part 1-2/8
-            float space = 1.5f;
+        } else {
+            float space = 2.0f;
             float bendRad = 0.40f * wireWidth;
+            // draw part 1-2/8
             paint.setStyle(Paint.Style.FILL);
             canvas.drawCircle(ax + space * xStep, ay + space * yStep, bendRad, paint);
             paint.setStyle(Paint.Style.STROKE);
-            canvas.drawLine(ax, ay, ax+space*xStep, ay+space*yStep, paint);
+            canvas.drawLine(ax, ay, ax + space * xStep, ay + space * yStep, paint);
+            // draw line |_ or it's part
+            drawDiagonal(canvas, a, b, paint, length);
             // draw part 7-8/8 only if applicable
-            if (length==full_wire_size) {
+            if (length == full_wire_size) {
                 paint.setStyle(Paint.Style.FILL);
-                canvas.drawCircle(bx - space*xStep, by - space*yStep, bendRad, paint);
+                canvas.drawCircle(bx - space * xStep, by - space * yStep, bendRad, paint);
                 paint.setStyle(Paint.Style.STROKE);
-                canvas.drawLine(bx - space*xStep, by - space * yStep, bx, by, paint);
+                canvas.drawLine(bx - space * xStep, by - space * yStep, bx, by, paint);
             }
-            // draw circle where it bends
-            if (length > 3) {
-                paint.setStyle(Paint.Style.FILL);
-                canvas.drawCircle(ax + space * xStep, by - space * yStep, bendRad, paint);
-                paint.setStyle(Paint.Style.STROKE);
-                // draw line down
-                canvas.drawLine(ax + space * xStep, ay + space * yStep, ax + space * xStep, by - space * yStep, paint);
-                // draw line right
-                canvas.drawLine(ax + space * xStep, by - space * yStep, bx - space * xStep, by - space * yStep, paint);
-            }
+        }
+    }
+
+    private void drawDiagonal(Canvas canvas, int a, int b, Paint paint, int length) {
+        float ax = canvasX(IntCoord.x(a));
+        float ay = canvasY(IntCoord.y(a));
+        float bx = canvasX(IntCoord.x(b));
+        float by = canvasY(IntCoord.y(b));
+        float xStep = (bx - ax) / full_wire_size;
+        float yStep = (by - ay) / full_wire_size;
+        float space = 2.0f;
+        float bendRad = 0.40f * wireWidth;
+        int shape = getDiagonalWireShape(a, b);
+        float minX = Math.min(ax + space * xStep, bx - space * xStep);
+        float maxX = Math.max(ax + space * xStep, bx - space * xStep);
+        float minY = Math.min(ay + space * yStep, by - space * yStep);
+        float maxY = Math.max(ay + space * yStep, by - space * yStep);
+        float dx = (maxX + minX) / 2.0f;
+        float dy = (maxY + minY) / 2.0f;
+        if (shape < 0) {
+            dx = minX;
+            dy = maxY;
+        }
+        if (shape > 0) {
+            dx = maxX;
+            dy = minY;
+        }
+        // draw part 1 of the line (ax,bx)->(dx,dy)
+        if (length > 3) {
+            canvas.drawLine(ax + space * xStep, ay + space * yStep, dx, dy, paint);
+        }
+        // draw part 2 of the line (dx,dy)->(bx,by)
+        if (length > 4) {
+            paint.setStyle(Paint.Style.FILL);
+            canvas.drawCircle(dx, dy, bendRad, paint);
+            paint.setStyle(Paint.Style.STROKE);
+            // draw line right
+            canvas.drawLine(dx, dy, bx - space * xStep, by - space * yStep, paint);
         }
     }
 
@@ -158,13 +193,13 @@ public class BoardDrawing extends CanvasDrawing {
     }
 
     void drawConnectedWires(Canvas canvas, int at, List<Integer> toNodes, int size) {
-        for(Integer n: toNodes) {
+        for (Integer n : toNodes) {
             drawWire(canvas, at, n, connectedPaint(), size);
         }
     }
 
     void drawSpecial(Canvas canvas, Board board, int at, Bitmap bmp) {
-        drawConnectedWires(canvas, at, board.adjNodes(at), 3);
+        drawConnectedWires(canvas, at, board.adjNodes(at), conn_wire_size);
         drawBitmapAtNode(canvas, at, bmp);
     }
 
@@ -172,18 +207,36 @@ public class BoardDrawing extends CanvasDrawing {
         float cx = canvasX(IntCoord.x(at));
         float cy = canvasY(IntCoord.y(at));
         float size = this.cellSize / 2.0f;
-        RectF targetRect = new RectF(cx-size/2, cy-size/2, cx+size/2, cy+size/2);
+        RectF targetRect = new RectF(cx - size / 2, cy - size / 2, cx + size / 2, cy + size / 2);
         Rect rectSrc = new Rect(0, 0, bmp.getWidth(), bmp.getHeight());
         canvas.drawBitmap(bmp, rectSrc, targetRect, bmpPaint);
     }
 
     private Paint connectedPaint() {
         if (connectionPaint == null) {
-            int width = (int)wireWidth;
+            int width = (int) wireWidth;
             int color = connectionColor;
             connectionPaint = strokePaint(width, color);
         }
         return connectionPaint;
+    }
+
+    /**
+     * Returns diagonal shape between 2 nodes, has to be deterministic
+     * -1
+     * 0 : normal diagonal
+     * 1 :
+     */
+    private Integer getDiagonalWireShape(int node0, int node1) {
+        int minNode = java.lang.Math.min(node0, node1);
+        int maxNode = java.lang.Math.max(node0, node1);
+        int key = 100 * minNode + maxNode;
+        Integer shape = diagonalWires.get(key);
+        if (shape == null) {
+            shape = random.nextInt(3) - 1; // -1, 0, 1
+            diagonalWires.put(key, shape);
+        }
+        return shape;
     }
 
 }
